d31aed7956a0eedca2f97057cc5a2334aedef098
pderyuga <polina.deryuga@persuit.com>
2026-03-12 09:03:58 -0400

    feat: add repl function

--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			input := scanner.Text()
+			words := cleanInput(input)
+			if len(words) == 0 {
+				continue
+			}
+
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowercaseText := strings.ToLower(text)
